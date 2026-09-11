7bab052fe657f5a5049c8edfba3eac0f3cfb4156
muraoka2016 <felipemuraoka2016@outlook.com>
2026-09-11 19:32:22 -0300

    webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -2,55 +2,63 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
 
-module.exports = {
-  entry: "./src/index.js",
+module.exports = (env, argv) => {
+  // Identifica se está rodando em produção (Vercel) ou desenvolvimento (sua máquina)
+  const isProduction = argv.mode === "production";
 
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.[contenthash].js",
-    clean: true,
-  },
+  return {
+    entry: "./src/index.js",
 
-  devServer: {
-    static: path.resolve(__dirname, "dist"),
-    port: 3000,
-    open: true,
-    hot: true,
-  },
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      // Usa hashes apenas em produção para evitar problemas de cache no navegador
+      filename: isProduction ? "bundle.[contenthash].js" : "bundle.js",
+      publicPath: "/", // Garante caminhos corretos para assets na Vercel
+      clean: true,
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
+    devServer: {
+      static: path.resolve(__dirname, "dist"),
+      port: 3000,
+      open: true,
+      hot: true,
+    },
 
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-            sourceType: "unambiguous",
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              sourceType: "unambiguous",
+            },
           },
         },
-      },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
 
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./src/template.html",
+        filename: "index.html",
+      }),
+      new Dotenv({
+        path: "./.env",
+        systemvars: true,
+      }),
     ],
-  },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-      filename: "index.html",
-    }),
-    new Dotenv(),
-  ],
-
-  mode: "development",
+    mode: argv.mode || "development",
+  };
 };
